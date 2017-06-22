b53b6b78d5e66d6c285e742a16656bdd4d822743
tharindu.dilanka <tharindu.dilanka@hnbassurance.com>
2017-06-22 16:42:14 +0530

    vcxv

--- src/app/shared/services/bank/bank.service.ts
+++ src/app/shared/services/bank/bank.service.ts
@@ -5,38 +5,80 @@ import 'rxjs/Rx';
 import { Observable } from "rxjs/Observable";
 import { URL_CONST } from '../../config/url.constants';
 
+import { USER } from '../../config/user';
 
 @Injectable()
 export class BankService {
 
   constructor(private http: Http) { }
-
-  getBank() {
-    return this.http.get(URL_CONST.URL_PREFIX + 'api/Bank/get')
-      .map((response: Response) => response.json());
-
+  //getAllRecords
+  getBanks() {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', USER.USER_AUTH_TOKEN);
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(URL_CONST.URL_PREFIX + 'api/Bank/get', options)
+      .map((response: Response) => response.json())
+      .timeout(60000)
+      .catch((error: any) => {
+        this.handleError;
+        return Observable.throw(new Error(error.status))
+      });
   }
 
-  SaveBank(params) {
+  //getSingleRecord
+  getBank(id) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', USER.USER_AUTH_TOKEN);
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(URL_CONST.URL_PREFIX + 'api/Bank/get/id', options)
+      .map((response: Response) => response.json())
+      .timeout(60000)
+      .catch((error: any) => {
+        this.handleError;
+        return Observable.throw(new Error(error.status))
+      });
+  }
 
-    try {
-      let body = params;
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      //headers.append('Authorization', USER.USER_AUTH_TOKEN);
-      let postoptions = new RequestOptions({ headers: headers });
+  //post
+  saveBank(params) {
+    let body = params;
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', USER.USER_AUTH_TOKEN);
+    let postoptions = new RequestOptions({ headers: headers });
+    return this.http.post(URL_CONST.URL_PREFIX + 'api/Bank/SaveBank', body, postoptions)
+      .map((response: Response) => {
+        return response;
+      })
+      .timeout(60000)
+      .catch((error: any) => {
+        this.handleError;
+        return Observable.throw(new Error(error.status))
+      });
+  }
 
-      return this.http.post(URL_CONST.URL_PREFIX + 'api/Bank/AddBank', body, postoptions)
-        .map((response: Response) => JSON.stringify(response.json()))
-        .timeout(60000)
-        .catch((error: any) => {
-          //this.handleError;
-          return Observable.throw(new Error(error.status))
-        });
+  //put
+  updateBank(params) {
+    let body = params;
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', USER.USER_AUTH_TOKEN);
+    let postoptions = new RequestOptions({ headers: headers });
+    return this.http.post(URL_CONST.URL_PREFIX + 'api/Bank/UpdateBank', body, postoptions)
+      .map((response: Response) => {
+        return response;
+      })
+      .timeout(60000)
+      .catch((error: any) => {
+        this.handleError;
+        return Observable.throw(new Error(error.status))
+      });
+  }
 
-    } catch (error) {
 
-    }
 
+
+  private handleError(error: Response) {
+    console.error('Error occured - ', error);
+    return Observable.throw(error.status || ' error');
   }
 
 
