f6efda725d798f245b42170104aeaabccc306001
tharindu.dilanka <tharindu.dilanka@hnbassurance.com>
2017-06-22 16:41:56 +0530

    dd

--- src/app/shared/models/Bank.models.ts
+++ src/app/shared/models/Bank.models.ts
@@ -1,5 +1,9 @@
-export interface IBank{
-    ID:number;
-	CODE:string;
-    DESCRIPTION:string;
+export interface IBank { 
+ Id:number  ;
+ Code:string  ;
+ Description:string  ;
+ CreatedBy:string  ;
+ CreatedDate:string  ;
+ EffectiveEndDate:string  ;
+ AccNo:string  ;
 }
