04986c01d4d4bfd8491e9b9dc07d24205b3257e4
Janaka Indrajith <j_indrajith@yahoo.com>
2017-07-05 13:47:21 +0530

    janaka

--- src/app/shared/config/url.constants.ts
+++ src/app/shared/config/url.constants.ts
@@ -9,14 +9,12 @@ export const URL_CONST = {
   //URL_PREFIX: 'http://localhost:55272/',
 
 
-
 //PRODUCTION
   URL_PREFIX: 'http://192.168.10.172:8085/',
 
 
-
-
  HOSTED_URL_PREFIX: 'http://192.168.10.172:8082/comm_docs',
+
 
 
 };
